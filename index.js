cc1d65ca7a0a6245ee7a313aaf0f7cdd22786ffe
Sean Christopher Febo <scf@techie.com>
2018-07-22 23:19:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-
+function loadIssue() {
+  let issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  };
+ 
+  let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+    for(let i=0; i<issues.length; i++) { 
+      
+  let result = template(issue);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
