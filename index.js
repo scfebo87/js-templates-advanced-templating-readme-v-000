640641274ae635a0b9b107b8e710aeeaada4b94e
Sean Christopher Febo <scf@techie.com>
2018-07-22 23:31:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,14 +10,11 @@ function loadIssues() {
   let result = template(issues);
   document.getElementsByTagName("main")[0].innerHTML += result;
 }
+
 Handlebars.registerHelper('comment_body', function() {
   if(this.state === "closed") {
-    return new Handlebars.SafeString(this.body)
+    return new Handlebars.SafeString(this.body);
   } else {
-    return new Handlebars.SafeString("<strong>" + this.body + "</strong>")
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>");
   }
-})
- 
-function loadIssues() {
-  //...
-}
+});
