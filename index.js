73a107bdf76432239080f9065de252643f67f7f5
Sean Christopher Febo <scf@techie.com>
2018-07-22 23:33:13 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
+function loadIssues() {
+  let issue = {
+    state: "closed",
+    number: 5,
+    created_at: "2016-03-31 16:23:13 UTC",
+    body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
+  };
+ 
+  let template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
+  let result = template(issues);
+  document.getElementsByTagName("main")[0].innerHTML += result;
+}
 
+Handlebars.registerHelper('comment_body', function() {
+  if(this.state === "closed") {
+    return new Handlebars.SafeString(this.body);
+  } else {
+    return new Handlebars.SafeString("<strong>" + this.body + "</strong>");
+  }
+});
